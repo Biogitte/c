b52228eba90ae06acbaaad73ff68ab92c6d53aae
Biogitte <birgittenilsson@live.dk>
2021-09-12 17:29:58 +0200

    Add paths to run data cleaning and preprocessing

--- global_env.sh
+++ global_env.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+# Working directory - remember to specify accordingly.
 export WORK_DIR=~/Desktop/code/data_science
 
+# Data directories
 export DATA=$WORK_DIR/data
 export DATA_RAW=$DATA/raw
 export DATA_PROC=$DATA/processed
 
+# Source code directory
 export SRC=$WORK_DIR/src
-export SRC_DATA=$SRC/data
-export SRC_MODELS=$SRC/models
-export SRC_TOOLS=$SRC/tools
 
-export DOCS=$WORK_DIR/docs
+# Executables directory
+export EXECUTE=$WORK_DIR/executables
 
+# Jupyter Notebooks directory
 export NOTEBOOKS=$WORK_DIR/notebooks
+
+# Variables related to fetching Kaggle data
+export FETCH_KAGGLE_DATA=$SRC/fetch_kaggle_data.py # the python script
+export KAGGLE_DATASET='fedesoriano/stroke-prediction-dataset' # the Kaggle dataset to download
+export KAGGLE_NEW_NAME='stroke_raw.csv' # the new name of the Kaggle data set
+
+# Variables related to data cleaning and preparation
+export CLEAN_DATA_SCRIPT=$SRC/clean_data.py # the python script
+export INPUT_FILE_PATTERN=$DATA_RAW'/*_'$KAGGLE_NEW_NAME # filename pattern with date as wild-card
+export UNWANTED_COLS='id,age,bmi' # columns to remove (like this: 'id,age,bmi')
+export LABEL_COL='stroke' # the column with classification labels
+export CLEAN_THE_DATA=$EXECUTE'/data_prep.sh' # the bash script to clean and prepare the data
+
+
+
